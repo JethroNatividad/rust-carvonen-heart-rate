765d7d0dcec0926bea98958fecffc8222c92130d
JethroNatividad <natividad.jet@gmail.com>
2024-01-28 20:23:03 +0800

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::Write;
+
 // A program that calculates your heart rate from 55% to 95%.
 // Input: Age, Resting heart rate
 // Process: loop from 55 - 95, increment by 5. Use carvonen formula to calculate heart rate.
