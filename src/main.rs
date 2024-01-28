7c971aa0e03a242ffa4df13592aa54d59eb3c6da
JethroNatividad <natividad.jet@gmail.com>
2024-01-28 18:39:02 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // A program that calculates your heart rate from 55% to 95%.
+// Input: Age, Resting heart rate
+// Process: loop from 55 - 95, increment by 5. Use carvonen formula to calculate heart rate.
+// Output: The intensity percentage and heart rate
 
 fn main() {
     println!("Hello, world!");
